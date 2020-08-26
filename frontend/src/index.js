71dd4591ecbba2fb9841c2de23456855e6441d2c
Thais Silveira <thais_lsilveira@hotmail.com>
2020-08-26 16:16:05 -0300

    Componentização

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,9 @@
-import { soma } from './soma';
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log(soma(1,5));
+import App from './App';
+
+
+// JSX? HTML dentro do JavaScript (Javascript XML)
+
+render(<App />, document.getElementById('app'));
