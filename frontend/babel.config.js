2a469eee91cd9c9d0a6adf87fa4e8093b66525b0
Thais Silveira <thais_lsilveira@hotmail.com>
2020-08-27 11:40:57 -0300

    Cadastrando Projetos

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
+  sourceType: "unambiguous",
   presets: [
     '@babel/preset-env',
     "@babel/preset-react"
   ],
+  plugins: [
+    '@babel/plugin-transform-runtime'
+  ]
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,11 +28,19 @@ function App() {
   // 2. Função para atualizarmos esse valor
 
 
-  function handleAddProject() {
+  async function handleAddProject() {
     //projects.push(`Novo projeto ${Date.now()}`)
 
-    setProjects([...projects, `Novo projeto ${Date.now()}` ]);
+    // setProjects([...projects, `Novo projeto ${Date.now()}` ]);
 
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Diego Fernandes"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
   
   return (
